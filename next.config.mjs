4c4628a411677754a55d66b7194eb33de784ff52
Suryansham <isitsuryansham@gmail.com>
2026-07-19 12:19:20 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 export default nextConfig;
+async rewrites() {
+  return [
+    {
+      source: "/",
+      has: [{ type: "host", value: "insightdashboard.finlead.ai" }],
+      destination: "/insight/india",
+    },
+  ];
+},
